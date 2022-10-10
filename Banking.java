9c10b22d4df0b6d4575f5ef5028a90fbf4d44879
Nikhil <nikhilbisht0026@gmail.com>
2022-10-10 11:27:48 +0530

    Initial class has been made

---
+++ Banking.java
@@ -0,0 +1,19 @@
+class Banking{
+    String name;
+    int age;
+    long aadhar;
+
+    public double withdraw(){
+        double balance;
+
+
+        return balance;
+    }
+
+    public double deposit(){
+        double balance;
+
+        return balance;
+    }
+
+}
